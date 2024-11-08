8de114f46fdf680b04a2a221bb9afde8f5cdd461
garylin2099 <yzlin@fuzhi.ai>
2024-11-08 14:50:47 +0800

    support use w/o template

--- metagpt/prompts/di/template.py
+++ metagpt/prompts/di/template.py
@@ -3,26 +3,16 @@ from metagpt.tools.libs.editor import FileBlock
 
 
 def read_file(file_path: str) -> str:
+    if not file_path.exists():
+        return ""
     with open(file_path, "r") as file:
         content = file.read()
     return FileBlock(path=str(file_path), content=content)
 
 
-INDEX_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "index.html")
-MAIN_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/main.jsx")
-APP_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/App.jsx")
-INDEX_CSS_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/index.css")
-CONFIG_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "vite.config.js")
-
-
-GENERAL_WEB_APP_TEMPLATE = f"""
-### Template Intro
-1. This is a template for gneral web app or game development. It is based on React framework with Tailwind CSS. The template includes the basic structure of a React project, including an index.html file and a src directory with an App.jsx file.
-2. The template is at {REACT_TEMPLATE_PATH.resolve().absolute()}
-3. Modify index.html, create new jsx files under src if needed, and rewrite src/App.jsx to meet the user's requirements.
-4. Style your elements with Tailwind CSS classes directly in the jsx files.
-
-### Project Structure
+TEMPLATE_PATH = REACT_TEMPLATE_PATH.resolve().absolute() if REACT_TEMPLATE_PATH.exists() else "N/A"
+TEMPLATE_STRUCTURE = (
+    """
 react_template/
 |-- eslint.config.js
 |-- index.html
@@ -36,6 +26,26 @@ react_template/
 |   `-- main.jsx
 |-- tailwind.config.js
 `-- vite.config.js
+"""
+    if TEMPLATE_PATH != "N/A"
+    else ""
+)
+INDEX_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "index.html")
+MAIN_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/main.jsx")
+APP_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/App.jsx")
+INDEX_CSS_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "src/index.css")
+CONFIG_CONTENT = read_file(REACT_TEMPLATE_PATH.resolve().absolute() / "vite.config.js")
+
+
+GENERAL_WEB_APP_TEMPLATE = f"""
+### Template Intro
+1. This is a template for gneral web app or game development. It is based on React framework with Tailwind CSS. The template includes the basic structure of a React project, including an index.html file and a src directory with an App.jsx file.
+2. The template is at {TEMPLATE_PATH}.
+3. Modify index.html, create new jsx files under src if needed, and rewrite src/App.jsx to meet the user's requirements.
+4. Style your elements with Tailwind CSS classes directly in the jsx files.
+
+### Project Structure
+{TEMPLATE_STRUCTURE}
 
 ### File Content
 #### index.html (Modify the title)
