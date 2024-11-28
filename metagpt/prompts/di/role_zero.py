1bc6fee327e8746a79bbd10c058d30c803fe0aa3
朱春临 <zhuchunlin@fuzhi.ai>
2024-11-28 09:50:35 +0800

    非阻塞pnpm对所有模板生效

--- metagpt/prompts/di/role_zero.py
+++ metagpt/prompts/di/role_zero.py
@@ -209,7 +209,6 @@ Response Category: TASK.
 10. Request: "Help me make a personal business card."
 Thought: The user is requesting assistance in creating a personal business card, which involves design and layout tasks.
 Response Category: TASK
-
 """
 QUICK_RESPONSE_SYSTEM_PROMPT = """
 {role_info}
