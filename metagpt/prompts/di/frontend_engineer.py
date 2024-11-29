c4aad9d80e02b73c630b72c9a73d0b83d4a1ab5c
朱春临 <zhuchunlin@fuzhi.ai>
2024-11-29 15:13:01 +0800

    修复智能体在找不到模板的情况下，从存放默认模板的位置把模板移动出来的bug {         "command_name": "Terminal.run_command",         "args": {             "cmd": "mv /template/default/react_template snake-game && cd snake-game"         }     }

--- metagpt/prompts/di/frontend_engineer.py
+++ metagpt/prompts/di/frontend_engineer.py
@@ -6,15 +6,14 @@ You have been tasked with developing a web app or game.
 If the user or a system design specifies otherwise, you should use a React template with Tailwind CSS. The template helps you get started, see the Template section for more information.
 1. Preparation: When provided a system design, read it first with Editor.read in a single response without any other commands. After reading, clearly indicate what files are instructed by the system design, then adhere to the design in your implementation. You may skip this step if no system design is provided.
 2. Use Engineer2.write_new_code to create new code files or rewrite code files. Plan out all files and call write_new_code only once for all files. Make sure you include all files listed in the system design if given.
-3. Navigate to your workspace, use ```mv {{template_path}} {{project_name}} && cd {{project_name}} ```.
-4. Write out every code detail, DON'T leave TODO or PLACEHOLDER.
-5. Editor is used to edit a small part of a file. You may edit multiple files in one response, but each file is allowed ONLY one operation. DON'T include the row number in the code generated or in the string your want to replace, they are there just for you to understand the position.
-6. When using Editor.edit_file_by_replace, be mindful of white spaces and line breaks!
-7. Do NOT initiate multiple Editor.insert_content_at_line calls at the same time, since the line number will change starting with the first execution, making line number of the subsequent calls incorrect. Split the calls into separate responses. For the same reason, Editor.insert_content_at_line should NOT go behind Editor.edit_file_by_replace in the same response. Perform insert operation in a separate response.
-8. After finishing the project, use "pnpm i && pnpm run build" to build the project. Reinstall and rebuild each time you make changes to the project.
-9. Deploy the project to the public after you install and build the project, use the dist folder.
-10. Use correct file paths, mind any cd command, for the current directory will change after executing the cd command and applies to all commands after it.
-11. Regarding a type of question related to personal card development: if no additional user information has been provided, you should directly deploy the retrieved template without any modifications.
+3. Write out every code detail, DON'T leave TODO or PLACEHOLDER.
+4. Editor is used to edit a small part of a file. You may edit multiple files in one response, but each file is allowed ONLY one operation. DON'T include the row number in the code generated or in the string your want to replace, they are there just for you to understand the position.
+5. When using Editor.edit_file_by_replace, be mindful of white spaces and line breaks!
+6. Do NOT initiate multiple Editor.insert_content_at_line calls at the same time, since the line number will change starting with the first execution, making line number of the subsequent calls incorrect. Split the calls into separate responses. For the same reason, Editor.insert_content_at_line should NOT go behind Editor.edit_file_by_replace in the same response. Perform insert operation in a separate response.
+7. After finishing the project, use "pnpm i && pnpm run build" to build the project. Reinstall and rebuild each time you make changes to the project.
+8. Deploy the project to the public after you install and build the project, use the dist folder.
+9. Use correct file paths, mind any cd command, for the current directory will change after executing the cd command and applies to all commands after it.
+10. Regarding a type of question related to personal card development: if no additional user information has been provided, you should directly deploy the retrieved template without any modifications.
 
 ## Template
 {GENERAL_WEB_APP_TEMPLATE_PROMPT}
