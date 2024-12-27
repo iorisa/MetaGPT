1a90225f586200ff75c06defad9644e47930791d
zhuchunlin <zhuchunlin@fuzhi.ai>
2024-12-27 22:20:56 +0800

    fix

--- tests/metagpt/environment/mgx_env/run_mgx_env.py
+++ tests/metagpt/environment/mgx_env/run_mgx_env.py
@@ -9,18 +9,16 @@ import agentops
 from metagpt.environment.mgx.mgx_env import MGXEnv
 from metagpt.roles import Architect, Engineer, ProductManager, ProjectManager
 from metagpt.roles.di.data_analyst import DataAnalyst
-from metagpt.roles.di.frontend_engineer import FrontendEngineer
+from metagpt.roles.di.engineer2 import Engineer2
 from metagpt.roles.di.team_leader import TeamLeader
 from metagpt.schema import Message
-
-# import agentops
 
 
 async def main(requirement="", enable_human_input=False, use_fixed_sop=False, allow_idle_time=30):
     if use_fixed_sop:
         engineer = Engineer(n_borg=5, use_code_review=False)
     else:
-        engineer = FrontendEngineer()
+        engineer = Engineer2()
 
     env = MGXEnv()
     env.add_roles(
@@ -30,6 +28,7 @@ async def main(requirement="", enable_human_input=False, use_fixed_sop=False, al
             Architect(use_fixed_sop=use_fixed_sop),
             ProjectManager(use_fixed_sop=use_fixed_sop),
             engineer,
+            # QaEngineer(),
             DataAnalyst(),
         ]
     )
@@ -40,14 +39,9 @@ async def main(requirement="", enable_human_input=False, use_fixed_sop=False, al
         human_input_thread = send_human_input(env, stop_event)
 
     if requirement:
-        if "@Alex" in requirement:
-            user_defined_recipient = "Alex"
-            env.publish_message(
-                Message(content=requirement, send_to={user_defined_recipient}),
-                user_defined_recipient=user_defined_recipient,
-            )
-        else:
-            env.publish_message(Message(content=requirement))
+        env.publish_message(Message(content=requirement))
+        # user_defined_recipient = "Alex"
+        # env.publish_message(Message(content=requirement, send_to={user_defined_recipient}), user_defined_recipient=user_defined_recipient)
 
     allow_idle_time = allow_idle_time if enable_human_input else 1
     start_time = time.time()
@@ -169,36 +163,6 @@ CODING_REQ1 = "写一个java的hello world程序"
 CODING_REQ2 = "python里的装饰器是什么"
 CODING_REQ3 = "python里的装饰器是怎么用的，给我个例子"
 
-EXAMPLE = """
-# /root/code/TemplateRecommendation/MetaGPT/data/护肤品.csv 基于护肤品数据集,需要开发一个产品浏览网站。整体页面采用左侧菜单栏和右侧内容区的布局方式。目前左侧菜单栏只需要添加"数据概览"这一个选项。
-在数据概览页面中,用户可以通过"类型"、"品牌"和"皮肤类型"进行产品筛选。其中"皮肤类型"支持多选,包括“干油混合皮肤“、“干性皮肤”、“正常皮肤”、“油性皮肤”、“敏感肌”这五种类型。筛选后的数据需要以表格形式展示出来,表格中应包含产品名、价格、评分以及适用皮肤类型。其中价格和评分支持点击表头进行升序、降序以及默认排序，需要比较醒目。对于产品适合的皮肤类型,采用"✅"符号来标识 - 适合该皮肤类型则显示"✅"，不适合则显示空白。表格默认每页展示10条数据，支持分页浏览。产品名称支持点击交互,点击后弹窗显示该产品的详细成分信息。
-注意
-1. 网站的名称叫做：护肤品大全
-2. 网站的风格使用清新风格，但是得保障数据清晰可见
-3. David需要一同给一份数据说明给到Alex
-"""
-
-EXAMPLE = """
-@Alex
-我有一个月的工作时间记录CSV，包含任务、时长、类型、完成状态等信息。请基于这些数据制作一个工作时间分配看板，展示各类任务的时间投入情况。\n请基于以下要求设计并实现看板:\n1. 展示日常时间分布图。\n2. 提供任务类型占比统计。\n3. 显示完成度统计表。\n4. 展示每日专注度图。\n5. 提供工作时段热图。\n6. 提供报表导出功能\n\n下面是我的输入材料:
-/root/code/TemplateRecommendation/MetaGPT/data/工作时间记录.csv
-"""
-
-#
-# EXAMPLE = """
-# @Alex Help me design a personal business card webpage. My YouTube channel name is 'Garystech'.
-# """
-
-# EXAMPLE = """
-# ``` "/api/v1/admin/users": { "get": { "tags": [ "管理员模块" ], "summary": "获取用户列表", "operationId": "list_user_api_v1_admin_users_get", "parameters": [ { "name": "cur_page", "in": "query", "required": false, "schema": { "type": "integer", "exclusiveMinimum": 0, "default": 1, "title": "Cur Page" } }, { "name": "page_num", "in": "query", "required": false, "schema": { "type": "integer", "exclusiveMinimum": 0, "default": 20, "title": "Page Num" } }, { "name": "status", "in": "query", "required": false, "schema": { "anyOf": [ { "$ref": "#/components/schemas/UserStatus" }, { "type": "null" } ], "title": "Status" } } ], "responses": { "200": { "description": "Successful Response", "content": { "application/json": { "schema": { "$ref": "#/components/schemas/UserListOut" } } } }, "422": { "description": "Validation Error", "content": { "application/json": { "schema": { "$ref": "#/components/schemas/HTTPValidationError" } } } } } }, "patch": { "tags": [ "管理员模块" ], "summary": "修改用户信息", "operationId": "update_user_api_v1_admin_users_patch", "requestBody": { "required": true, "content": { "application/json": { "schema": { "$ref": "#/components/schemas/UserUpdateIn" } } } }, "responses": { "200": { "description": "Successful Response", "content": { "application/json": { "schema": { "$ref": "#/components/schemas/UserUpdateOut" } } } }, "422": { "description": "Validation Error", "content": { "application/json": { "schema": { "$ref": "#/components/schemas/HTTPValidationError" } } } } } } }, "UserStatus": { "type": "string", "enum": [ "active", "pending", "activating" ], "title": "UserStatus" }, ``` @Alex 实现一个管理员页面，用于通过用户申请请求，需要两个功能，查看用户列表支持pending或者active筛选用户 只有pending的用户需要激活 支持id或者email激活 API_BASE_URL和鉴权token 允许设置
-#
-# """
-
-# EXAMPLE = """
-# @Alex  Using react template develop a pretty personal card for me. My name is Alex, a traveller and vlogger, my tiktok and youtube account are both named alex_travel. On the page, put a main video block with this link (https://youtu.be/GZbU3zvTTG8?si=gCxmCdNyZ-Vdm9_Z) in the middle, which is my vlogger to Europe. Then search three images for Great Wall, Pyramids, and African animals and place them in a row under the video. Use a scenic snow view of the Mount Alps as the background.
-#
-# """
-
 
 if __name__ == "__main__":
     # NOTE: Add access_token to test github issue fixing
@@ -207,4 +171,4 @@ if __name__ == "__main__":
     #       Set enable_human_input to True if you want to simulate sending messages in chatbox
     agentops.init(api_key="", auto_start_session=False, skip_auto_end_session=True)
     session = agentops.start_session()
-    asyncio.run(main(requirement=EXAMPLE, enable_human_input=False, use_fixed_sop=False))
+    asyncio.run(main(requirement="写一个2048游戏", enable_human_input=False, use_fixed_sop=False))
