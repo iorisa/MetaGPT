6c4e3f0c07555ca2010c1ba01e6094a5dd294ea3
seehi <6580@pm.me>
2024-12-30 17:58:26 +0800

    update

--- metagpt/prompts/di/supabase.py
+++ metagpt/prompts/di/supabase.py
@@ -47,7 +47,7 @@ const {{ data: {{ session }} }} = await supabase.auth.getSession();
 const {{ data: {{ user }} }} = await supabase.auth.getUser();
 ```
 
-- Note that emailRedirectTo MUST be set to the current origin, THIS IS CRUCIAL for user registration:
+- NOTE that emailRedirectTo MUST be set to the current origin, THIS IS CRUCIAL for user registration:
 ```jsx
 const {{ error }} = await supabase.auth.signUp({{email, password, options: {{emailRedirectTo: window.location.origin}} }});
 ```
