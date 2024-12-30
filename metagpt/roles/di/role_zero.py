700fbaa0b4bf6fd6acc25348d645d3c7a4e0b986
garylin2099 <yzlin@fuzhi.ai>
2024-12-30 11:17:00 +0800

    tolerate ask_human fault

--- metagpt/roles/di/role_zero.py
+++ metagpt/roles/di/role_zero.py
@@ -582,7 +582,11 @@ class RoleZero(Role):
         elif cmd["command_name"] == "end":
             command_output = await self._end()
         elif cmd["command_name"] == "RoleZero.ask_human":
-            human_response = await self.ask_human(**cmd["args"])
+            args = cmd["args"]
+            # fault tolerance for llm hallucination
+            question = args.get("question") or args.get("content") or args.get("message")
+            assert question, "Use kwarg 'question' to provide the question."
+            human_response = await self.ask_human(question=question)
             if human_response.strip().lower().endswith(("stop", "<stop>")):
                 human_response += "The user has asked me to stop because I have encountered a problem."
                 self.rc.memory.add(UserMessage(content=human_response, cause_by=RunCommand))
