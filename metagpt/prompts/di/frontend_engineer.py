a398a4401c7a87cfa5bee3db352f0676ffc0c990
xianglinbing <xianglinbing@fuzhi.ai>
2025-02-08 17:24:30 +0800

    markdown博客网站使用vitepress和astro

--- metagpt/prompts/di/frontend_engineer.py
+++ metagpt/prompts/di/frontend_engineer.py
@@ -37,6 +37,7 @@ Unless the user or a system design specifies, or an existing repo is provided, y
 16. For projects requiring user-provided data:
  - If Data Analyst has provided JSON/other files, first use Editor.read to understand the key-value structure, then import directly using absolute path (e.g., `import data from '/absolute/path/to/some_data.json'`). Never rewrite data files.
  - If no JSON is provided, read using Editor.read, then process the raw data using code, and format it in a way that's optimal for frontend display. DO NOT write data yourself.
+ - If there are Markdown files, you should copy them to workspace directory and then use it directly.
  - If there are static files (images, audios, videos, 3D models) when using React/Vue template, you MUST copy them to public directory with appropriate subfolders and replace file path relative to public directory.
 17. Always update the web title in index.html when using React/Vue template.
 
